f1efdddcd45172392770f2dafb620c80262e557b
Megan Kakitsubo <megankaki@gmail.com>
2015-06-18 14:41:06 -0700

    added users model and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:address, :city, :state, :zipcode)
+    
+  end
+end
