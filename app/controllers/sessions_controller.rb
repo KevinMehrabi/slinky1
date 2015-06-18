6adfa3742a584dec5e655c2fcfb5afcc18fc1140
Megan Kakitsubo <megankaki@gmail.com>
2015-06-17 22:23:01 -0700

    moved the end up to make code DRY

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-      @auth = User.from_omniauth(request.env['omniauth.auth'])
-      session['auth'] = @auth
-      redirect_to sessions_show_path
+    @auth = User.from_omniauth(request.env['omniauth.auth'])
+    session['auth'] = @auth
+    redirect_to sessions_show_path
   end
 
   def destroy
       session['auth'] = nil
       redirect_to root_path
   end
-
-
-
 end
