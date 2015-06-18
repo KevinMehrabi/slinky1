b8b83f2d12209e891c6e76ad0393e5730dc9754c
Megan Kakitsubo <megankaki@gmail.com>
2015-06-17 22:51:00 -0700

    set user to equal to find_or_create_by function. passed in uid to check whether data exists, and provider to create if new user. this saves the user api data to active records

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
 	class << self
 		def from_omniauth(auth_hash)
-			
+			user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
 		end
 		
 	end
