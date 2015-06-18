2d753f252b32d2a591e7293f29796da4ff287070
Megan Kakitsubo <megankaki@gmail.com>
2015-06-17 23:01:24 -0700

    pulled nickname from the info hash and put it into user as usernmae

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 			user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
 			user.name = auth_hash["info"]['name']
 			user.email = auth_hash["info"]['email']
+			user.username = auth_hash["info"]['nickname']
 		end
 		
 	end
