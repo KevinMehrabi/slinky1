e478732a5a78f410c0d03338c46115b589121fb1
Megan Kakitsubo <megankaki@gmail.com>
2015-06-18 14:09:07 -0700

    add geocoder gem and migrated latitude and longitude columns to user model

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'omniauth'
 
 gem 'figaro'
+gem 'geocoder'
 
 gem 'omniauth-linkedin-oauth2'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618071444) do
+ActiveRecord::Schema.define(version: 20150618203525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20150618071444) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "country"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
