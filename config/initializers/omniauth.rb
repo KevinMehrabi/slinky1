af91db8189092c6e120e64b8c9ff6b9db6db23e7
Megan Kakitsubo <megankaki@gmail.com>
2015-06-17 23:45:02 -0700

    added auth_failure parameter in the SessionsController.action for omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
+	OmniAuth.config.on_failure = Proc.new do |env|
+		SessionsController.action(:auth_failure).call(env)
+	end
 end
