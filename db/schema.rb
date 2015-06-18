4e8eb71c8c900961f6ccd3e0687b2a3535d94dd2
Megan Kakitsubo <megankaki@gmail.com>
2015-06-17 21:25:54 -0700

    added email and username columns to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618001732) do
+ActiveRecord::Schema.define(version: 20150618031640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20150618001732) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
+    t.string   "username"
   end
 
 end
