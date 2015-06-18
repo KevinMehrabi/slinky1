94bfe0beb02834e5ade51e8f68c4f619486076da
Megan Kakitsubo <megankaki@gmail.com>
2015-06-18 14:38:23 -0700

    added routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
+  get 'users/edit'
+
+  get 'users/update'
+
   get 'sessions/new'
 
   get 'sessions/create'
