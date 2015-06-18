69e753388585539942302e94278073ad7955d823
Megan Kakitsubo <megankaki@gmail.com>
2015-06-17 23:58:42 -0700

    pulled first_name from the info hash and put it into user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 		def from_omniauth(auth_hash)
 			user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
 			user.name = auth_hash["info"]['name']
+			user.first_name = auth_hash["info"]['first_name']
 			user.email = auth_hash["info"]['email']
 			user.username = auth_hash["info"]['nickname']
 			user.location = auth_hash['info']['location']['name']
