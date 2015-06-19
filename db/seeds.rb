96062a26528d9cf53384797650925bba418b09d5
Kevin Mehrabi <kmehrabi@me.com>
2015-06-19 15:14:26 -0700

    fixed seeds to load latitude and logitude columns with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,14 @@ users = User.create ([  {provider: "linkedin", uid: "95evirp5qy", name: "Megan K
                         image_url: "https://media.licdn.com/mpr/mprx/0_YCazetsTQH7rRx-MjNZN_8HTQ2arY7O4ACszaQY8QgplYxhNN1nv_APT5SAlOpPzACqREnr37USAjw-qcAId8QADoUSKjwk4NAI9wbXh3sZpcgxk48gZI1oAEaB8Nw1djt7J2MBTbit",
                         url: "https://www.linkedin.com/pub/megan-kakitsubo/65/344/62b", created_at: "2015-06-19 17:59:55", updated_at: "2015-06-19 18:04:40",
                         email: "megankaki@gmail.com", username: "Megan Kakitsubo", first_name: "Megan", last_name: "Kakitsubo", country: "us",
-                        latitude: nil, longitude: nil, address: nil, city: nil, state: nil, zipcode: nil,
+                        latitude: '34.0131295', longitude: '-118.49516549999998', address: nil, city: nil, state: nil, zipcode: nil,
                         current_location: "['34.0131295', '-118.49516549999998']" },
 
                         {provider: "linkedin", uid: "a6Lnixk4BC", name: "Daley Meistrell", location: "Greater Los Angeles Area",
                         image_url: "https://media.licdn.com/mpr/mprx/0_N-oHBQTmFXd7gIjpN9sUBX12F3f00eyp9zOBBXGO4bRfh2wy4qVXJknrd27hy7grvAH9Z_y6U3Py",
                         url: "https://www.linkedin.com/pub/daley-meistrell/50/307/11a",
                         created_at: "2015-06-18 16:08:26", updated_at: "2015-06-19 17:29:44", email: "daley@bodyglove.com", username: "Daley Meistrell",
-                        first_name: "Daley", last_name: "Meistrell", country: "us", latitude: nil, longitude: nil, address: nil, city: nil, state: nil, zipcode: nil,
+                        first_name: "Daley", last_name: "Meistrell", country: "us", latitude: '34.0131608', longitude: '-118.4951012', address: nil, city: nil, state: nil, zipcode: nil,
                         current_location: "[\"34.0131608\", \"-118.4951012\"]"},
 
 
@@ -26,7 +26,7 @@ users = User.create ([  {provider: "linkedin", uid: "95evirp5qy", name: "Megan K
                         image_url: "https://media.licdn.com/mpr/mprx/0_Srapvx9AsTBXtX417qd7vpLtV_-drbM17cV_vpQfLQ64fimP39OrRy-3qDtcP8Z0u12GsUZy12uV",
                         url: "https://www.linkedin.com/in/kmehrabi",
                         created_at: "2015-06-19 17:09:36", updated_at: "2015-06-19 18:47:19", email: "Kevin.Mehrabi@gmail.com", username: "Kevin Mehrabi",
-                        first_name: "Kevin", last_name: "Mehrabi", country: "us", latitude: nil, longitude: nil, address: nil, city: nil, state: nil, zipcode: nil,
+                        first_name: "Kevin", last_name: "Mehrabi", country: "us", latitude: '34.0131714', longitude: '-118.49512530000001', address: nil, city: nil, state: nil, zipcode: nil,
                         current_location: "[\"34.0131714\", \"-118.49512530000001\"]"},
 
                       ])
