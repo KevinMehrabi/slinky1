a814e36cd947a8061757027a325c546a11e5a511
Megan Kakitsubo <megankaki@gmail.com>
2015-06-19 17:23:34 -0700

    add rescue into api call for sessions controller create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # begin
+    begin
       @user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = @user.id
       current_user
       save_user_location
       flash[:success] = "Welcome, #{@user["first_name"]}!"
-      # Rails.logger @auth
-    # rescue
-    #   flash[:warning] = "There was an error while trying to authenticate you..."
-    # end
+    rescue
+      flash[:warning] = "There was an error while trying to authenticate you..."
+    end
   end
 
   def destroy
