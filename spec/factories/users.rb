cd5cfe713525ecda0739137e22c11d1aab97b811
Megan Kakitsubo <megankaki@gmail.com>
2015-06-23 11:29:13 -0700

    made factorygirl test for users model spec for first_name, last_name, email, latitude, longitude, description, provider, image_url, country, url

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    first_name "Megan"
+    first_name "Joe"
+    last_name "Random"
+    email "joe@random.com"
+    latitude 34
+    longitude 74
+    description "blah blah"
+    provider "linkedin"
+    image_url "https://media.licdn.com/blah/blah"
+    country "us"
+    url "https://www.linkedin.com/pub/random"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,12 +9,85 @@ RSpec.describe User, type: :model do
 
     describe "users" do
     	# Fields come from the factory file
-        it 'should have a defaulted fields' do
+        it 'should have a defaulted first_name field' do
             user = FactoryGirl.build(:user)
             print user
-            expect(user.first_name).to eq "Megan" # What should this equal?
+            expect(user.first_name).to eq "Joe"
         end
     end
+    describe "users" do
+        # Fields come from the factory file
+        it 'should have a defaulted last_name field' do
+            user = FactoryGirl.build(:user)
+            print user
+            expect(user.last_name).to eq "Random"
+        end
+    end
+    describe "users" do
+        # Fields come from the factory file
+        it 'should have a defaulted email field' do
+            user = FactoryGirl.build(:user)
+            print user
+            expect(user.email).to eq "joe@random.com"
+        end
+    end
+    describe "users" do
+        # Fields come from the factory file
+        it 'should have a defaulted latitude field' do
+            user = FactoryGirl.build(:user)
+            print user
+            expect(user.latitude).to eq 34
+        end
+    end
+    describe "users" do
+        # Fields come from the factory file
+        it 'should have a defaulted longitude field' do
+            user = FactoryGirl.build(:user)
+            print user
+            expect(user.longitude).to eq 74 
+        end
+    end
+    describe "users" do
+        # Fields come from the factory file
+        it 'should have a defaulted description field' do
+            user = FactoryGirl.build(:user)
+            print user
+            expect(user.description).to eq "blah blah"
+        end
+    end
+    describe "users" do
+        # Fields come from the factory file
+        it 'should have a defaulted provider field' do
+            user = FactoryGirl.build(:user)
+            print user
+            expect(user.provider).to eq "linkedin" 
+        end
+    end
+    describe "users" do
+        # Fields come from the factory file
+        it 'should have a defaulted image_url field' do
+            user = FactoryGirl.build(:user)
+            print user
+            expect(user.image_url).to eq "https://media.licdn.com/blah/blah"
+        end
+    end
+    describe "users" do
+        # Fields come from the factory file
+        it 'should have a defaulted country field' do
+            user = FactoryGirl.build(:user)
+            print user
+            expect(user.country).to eq "us"
+        end
+    end
+    describe "users" do
+        # Fields come from the factory file
+        it 'should have a defaulted url field' do
+            user = FactoryGirl.build(:user)
+            print user
+            expect(user.url).to eq "https://www.linkedin.com/pub/random"
+        end
+    end
+
     describe "#full_address" do
     	it "should return address seperated by comma" do
     		user = FactoryGirl.build_stubbed(:user, address: "483 Gayley Avenue", city: "Los Angeles", state: "CA", zipcode: "90024")
@@ -22,5 +95,6 @@ RSpec.describe User, type: :model do
     		expect(user.full_address).to eq "483 Gayley Avenue, Los Angeles, CA, 90024"
     	end
     end
+
 
 end
