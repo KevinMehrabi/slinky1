5ecd5b5dcc4c1ac3a18e82f07a6aa2d82b346cd7
Kevin Mehrabi <kmehrabi@me.com>
2015-06-25 17:13:04 -0700

    migrates table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626000821) do
+ActiveRecord::Schema.define(version: 20150626001132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "mark_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
