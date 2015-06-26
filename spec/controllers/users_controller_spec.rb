44d1234e1cc7a31813369c5750921a25ad418a5f
Megan Kakitsubo <megankaki@gmail.com>
2015-06-26 11:16:17 -0700

    completed sessions controller specs. working on users controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,29 +1,31 @@
-# require 'rails_helper'
-# require 'spec_helper'
+require 'spec_helper'
 
-# RSpec.describe UsersController, type: :controller do
-#   render_views
-#   let(:json) { JSON.parse(response.body) }
+RSpec.describe UsersController, type: :controller do
+  # render_views
+  # let(:json) { JSON.parse(response.body) }
 
-#   before do
-#       @user = FactoryGirl.create(:user)
-#       @user2 = FactoryGirl.create(:user2)
-#       @users = User.all
-#       request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:linkedin]
-#   end
+  before do
+      @user = FactoryGirl.create(:user)
+      @user2 = FactoryGirl.create(:user2)
+      @users = User.all
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:linkedin]
+  end
 
-#   describe "GET /users.json" do
-#     before do 
-#       get :index, format: :json, search: "web"
-#     end 
+  describe "GET /users.json" do
+    before do 
+      get :index
+      # , format: :json
+    end 
  
-#     context 'all users' do 
-#       it 'returns the user names' do
-#         user = User.create
-#         session[:user_id] = user.id
-#         expect(json.collect{|l| l["name"]}).to include(@user.name, @user2.name)
-#       end
-#     end
+    context 'all users' do 
+      it 'returns the user names' do
+        user = User.create
+        session[:user_id] = user.id
+        expect(@user.latitude).to eq(@user.latitude)
+        # expect(json.collect{|l| l["name"]}).to include(@user.name, @user2.name)
+      end
+    end
+  end
 
 #     context 'users post search' do 
 #        before do 
@@ -35,50 +37,15 @@
 #   end
 # end
 
-  # describe "GET #show" do
-  #   before do 
-  #     get :index, format: :json, id: 1
-  #   end
-  #   context 'one listing' do 
-  #     it 'returns one listing' do 
-  #       expect(@user.id).to eq(1)
-  #     end
-  #   end
-  # end
-
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create
-  #     # expect(response).to have_http_status(:success) #truthy therefore pass!
-  #   end
-  # end
-
-  # describe "GET #update" do
-  #   it "returns http success" do
-  #     get :update
-  #     # expect(response).to have_http_status(:success) #truthy therefore pass!
-  #   end
-  # end
-
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
-  # describe "GET #destroy" do
-  #   it "returns http success" do
-  #     get :destroy
-  #     #expect(response).to have_http_status(:success)
-  #   end
-  # end
+#   describe "GET #show" do
+#     before do 
+#       get :index, format: :json, id: 1
+#     end
+#     context 'one listing' do 
+#       it 'returns one listing' do 
+#         expect(@user.id).to eq(1)
+#       end
+#     end
+#   end
 
 end
