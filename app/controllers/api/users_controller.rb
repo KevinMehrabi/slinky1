36575a088ea63cc1d7e8ccfef4712ccab9b87420
Megan Kakitsubo <megankaki@gmail.com>
2015-06-29 15:32:31 -0700

    adds api folder in controllers

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,13 @@
+module API
+	class UsersController < ApplicationController
+	protect_from_forgery with: :null_session
+	
+		def index
+			render json: User.all
+		end
+
+		def show
+			render json: User.find(params[:id])
+		end
+	end
+end
